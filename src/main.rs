d4d34b5f7d4fe51d2d1daaa8f14dbcd11ab0e2e9
Gabriel <gebv0896@gmail.com>
2018-08-29 12:29:33 -0600

    Actualizando un poco el trabajo sobre el Abstract Factory

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![allow(non_snake_case)]
 include!("Facade.rs");
-
+include!("AbstFactory.rs");
 
 fn main() {
 
@@ -13,13 +13,24 @@ fn main() {
     println!("###Encendiendo piloto automatico###\n");
 
 
-    let p=FacadePiloto{y:true};
-    p.validacion();
+    let a=FacadePiloto{y:true};
+    a.validacion();
 
     print!("\n");
     println!("###Apagando piloto automatico###\n");
 
-    let p=FacadePiloto{y:false};
-    p.validacion();
+    let a=FacadePiloto{y:false};
+    a.validacion();
+
+    print!("\n");
+    print!("\n");
+
+
+    //*********Utilizando el patron ABSTRACT FACTORY********
+
+    let b=abstract_factory{y:1};
+    b.elegir_orden();
+
+
 }
 
